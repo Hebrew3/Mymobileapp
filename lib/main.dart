622dc3ecde5984224159d092a82fafd7a342878d
Hebrew3 <ddeasis1990@gmail.com>
2025-06-19 10:19:36 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,66 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         title: Text('BookMyShoot'),
-        backgroundColor: Colors.lightBlueAccent[300],
+        backgroundColor: Color.fromARGB(255, 72, 48, 255),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text('Shoot'),
-            Text('Photo'),
-            Text('Visualize'),
-          ]
+      body: Column(
+      
+        children: [
+
+          Container(
+            margin: EdgeInsets.all(20),
+            color: Color.fromARGB(255, 72, 48, 255),
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Text(
+                  'Name: ',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Daniel De Asis',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+            ],
+          ),
         ),
+         Container(
+            margin: EdgeInsets.all(20),
+            color: Color.fromARGB(255, 72, 48, 255),
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Text(
+                  'Age: ',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  '21 yrs old',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+            ],
+          ),
+        ),
+         Container(
+            margin: EdgeInsets.all(20),
+            color: Color.fromARGB(255, 72, 48, 255),
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Text(
+                  'Gender: ',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Male',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+            ],
+          ),
+        ),
+        ],
       ),
+
     ),
   ));
 }
